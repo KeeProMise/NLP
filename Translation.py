f9e90a0f15086c1845bc705d8f81c0f117dd22cf
keePromise <zhangjian@iie.cas.cn>
2020-10-28 10:30:24 +0800

    机器翻译

--- Translation.py
+++ Translation.py
@@ -11,7 +11,7 @@ from torch import optim
 import torch.nn.functional as F
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+# device = "cpu"
 
 
 SOS_token = 0
@@ -337,11 +337,12 @@ def showPlot(points):
     ax.yaxis.set_major_locator(loc)
     plt.plot(points)
 
-# hidden_size = 256
-# encoder1 = EncoderRNN(input_lang.n_words, hidden_size).to(device)
-# attn_decoder1 = AttnDecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.1).to(device)
-#
-# trainIters(encoder1, attn_decoder1, 75000, print_every=50)
+hidden_size = 256
+encoder1 = EncoderRNN(input_lang.n_words, hidden_size).to(device)
+attn_decoder1 = AttnDecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.1).to(device)
+
+trainIters(encoder1, attn_decoder1, 75000, print_every=50)
+
 encoder = torch.load(".\TranslationModel\encoder")
 decoder = torch.load(".\TranslationModel\Anndecoder")
 
