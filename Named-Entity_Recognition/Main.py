9642049383faf09eba18a09bb53a5181bc9b3f13
keePromise <zhangjian@iie.cas.cn>
2020-10-30 19:25:57 +0800

    增加模型估算

--- Named-Entity_Recognition/Main.py
+++ Named-Entity_Recognition/Main.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 import torch.optim as optim
 
 
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 device = "cpu"
 embeding_dim = 50
 hidden_dim = 256
@@ -24,6 +24,7 @@ tagvocab = DataSet.vocab(tags)
 
 tagvocab[0][START_TAG] = len(tagvocab[0])
 tagvocab[0][STOP_TAG] = len(tagvocab[0])
+rawvocab[0]["<unk>"] = len(rawvocab[0])
 
 
 DataSet.save_vocab(rawvocab,".\\vocab","train_rawvocab")
@@ -31,9 +32,10 @@ DataSet.save_vocab(tagvocab,".\\vocab","train_tagvocab")
 
 trainset = DataSet.trainDataSet(inputs,tags,rawvocab,tagvocab,device)
 
-model = BiLSTM_CRF.BiLSTM_CRF(len(rawvocab[0]),tagvocab[0],embeding_dim,hidden_dim)
+# model = BiLSTM_CRF.BiLSTM_CRF(len(rawvocab[0]),tagvocab[0],embeding_dim,hidden_dim)
+model = torch.load(".\model\BiLSTM-CRF2")
 model.to(device)
-optimizer = optim.SGD(model.parameters(), lr=0.01, weight_decay=1e-4)
+optimizer = optim.SGD(model.parameters(), lr=0.05, weight_decay=1e-4)
 
 import time
 import math
@@ -77,7 +79,7 @@ def train(trainset,epoch,print_every):
                 print_loss_total = 0
                 print('%s (%d %d%%) %.4f' % (timeSince(start, iter / (iters*epoch)),
                                              iter, iter / (iters*epoch) * 100, print_loss_avg))
-    torch.save(model,".\model\BiLSTM-CRF1")
+    torch.save(model,".\model\BiLSTM-CRF3")
 
 def main():
     train(trainset, 2, 50)
--- Named-Entity_Recognition/testModel.py
+++ Named-Entity_Recognition/testModel.py
@@ -2,23 +2,22 @@ import torch
 import DataSet
 import BiLSTM_CRF
 import numpy as np
-
+import sklearn.metrics as score
+import evaluation
 
 # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 device = "cpu"
 
-rawfile = ".\data\\train_corpus.txt"
-lablefile = ".\data\\train_label.txt"
+rawfile = ".\data\\test_corpus.txt"
 
 inputs = DataSet.PreparaData(rawfile)
-tags = DataSet.PreparaData(lablefile)
 
 rawvocab = DataSet.load_vocab(".\\vocab","train_rawvocab")
 tagvocab = DataSet.load_vocab(".\\vocab","train_tagvocab")
 
-testset = DataSet.trainDataSet(inputs,tags,rawvocab,tagvocab,device)
+testset = DataSet.testDataSet(inputs,rawvocab,device)
 
-model = torch.load(".\model\BiLSTM-CRF1")
+model = torch.load(".\model\BiLSTM-CRF2")
 model.to(device)
 
 import time
@@ -28,15 +27,17 @@ def test(testset):
     iters = len(testset)
     start = time.time()
     tag_list = []
+    y_true = []
+
     now = 0
     with torch.no_grad():
-        for iter, _ in testset:
-
+        for iter in testset:
             score,perdict = model(iter)
             tag_list.append(perdict)
             if now%500 == 0:
                 nowtime = time.time()
                 print(nowtime-start)
+
     return(tag_list)
 
 def tagi2label(tag_list,tagvocab):
@@ -53,11 +54,19 @@ def write2fileSystem(filepath,labellist):
         str = str+"\n"
         file_list.append(str)
     file_handle.writelines(file_list)
-def main():
-    tag_list=test(testset)
-    label_list =tagi2label(tag_list,tagvocab)
-    write2fileSystem(".\data\mytrain_label.txt", label_list)
 
+def evaluate(predict_filepath,true_filepath):
+    patten = [("B-PER", "I-PER"), ("B-LOC", "I-LOC"), ("B-ORG", "I-ORG")]
+    predict_label = DataSet.PreparaData(predict_filepath)
+    true_label = DataSet.PreparaData(true_filepath)
+    evaluation.NER_score(patten, true_label, predict_label)
+
+
+def main():
+    # tag_list=test(testset)
+    # label_list =tagi2label(tag_list,tagvocab)
+    # write2fileSystem(".\data\mytest_label.txt", label_list)
+    evaluate(".\data\mytest_label.txt", ".\data\\test_label.txt")
 
 if __name__ == '__main__':
     main()
